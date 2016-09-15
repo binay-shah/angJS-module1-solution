960be39fa843960aa6a0a19c46a5c2e5d27d7ffa
Binay <binay18.shah@gmail.com>
2016-09-15 09:32:58 +0530

    commit2

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 (function(){
   angular.module('LunchCheck', [])
 
-  .controller('n', function ($scope) {
+  .controller('LunchCheckController', function ($scope) {
     $scope.food_items ="";
     $scope.displayMessage = function(){
       var food_arr = $scope.food_items.split(',');
-      console.log(food_arr.length)
-      if (food_arr.length  <= 3)
+      console.log(food_arr)
+      if (food_arr[0]  == "" )
+        $scope.message = "Please enter data first"
+      else if (food_arr.length  >= 1 && food_arr.length  <= 3)
         $scope.message = "Enjoy!"
       else {
           $scope.message = "Too much!"
